7fb11cf9def1b1423a5db258e42d14f722121e2d
Tatsuya9156 <hmty9156@gmail.com>
2021-01-22 12:20:50 +0900

    devise導入及びルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :workers, controllers: {
+    sessions:      'workers/sessions',
+    passwords:     'workers/passwords',
+    registrations: 'workers/registrations'
+  }
+  devise_for :employees, controllers: {
+    sessions:      'employees/sessions',
+    passwords:     'employees/passwords',
+    registrations: 'employees/registrations'
+  }
   root to: 'customers#index'
 end
