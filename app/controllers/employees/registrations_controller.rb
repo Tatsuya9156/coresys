84e732bb864f7b5d40356beddc28c4bea97a8a72
Tatsuya9156 <hmty9156@gmail.com>
2021-01-25 12:40:29 +0900

    社員新規登録ページの修正

--- app/controllers/employees/registrations_controller.rb
+++ app/controllers/employees/registrations_controller.rb
@@ -42,7 +42,7 @@ class Employees::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :name_kana, :phone])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :name_kana, :phone, :admin])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
--- db/migrate/20210122025549_devise_create_employees.rb
+++ db/migrate/20210122025549_devise_create_employees.rb
@@ -9,6 +9,8 @@ class DeviseCreateEmployees < ActiveRecord::Migration[6.0]
       t.string  :phone,              null: false
       t.string  :email,              null: false, default: ""
       t.string  :encrypted_password, null: false, default: ""
+      t.boolean :admin,              null: false
+      
 
       ## Recoverable
       t.string   :reset_password_token
