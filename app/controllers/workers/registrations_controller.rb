7510cd859d8008347c51ebf302b4dfcdb46ef99f
Tatsuya9156 <hmty9156@gmail.com>
2021-01-26 15:38:20 +0900

    職人登録、編集機能の実装

--- app/controllers/workers/registrations_controller.rb
+++ app/controllers/workers/registrations_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Workers::RegistrationsController < Devise::RegistrationsController
+  prepend_before_action :require_no_authentication, only: [:cancel]
+  before_action :creatable?, only: [:new, :create]
   before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
@@ -49,6 +51,22 @@ class Workers::RegistrationsController < Devise::RegistrationsController
   # def configure_account_update_params
   #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
   # end
+
+  def current_employee_is_admin?
+    current_employee.admin == true
+  end
+
+  def sign_up(resource_name, resource)
+    if !current_employee_is_admin?
+      sign_in(resource_name, resource)
+    end
+  end
+
+  def creatable?
+    if !current_employee_is_admin?
+      redirect_to root_path
+    end
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
--- app/controllers/workers_controller.rb
+++ app/controllers/workers_controller.rb
@@ -2,4 +2,23 @@ class WorkersController < ApplicationController
   def index
     @workers = Worker.all
   end
+
+  def edit
+    @worker = Worker.find(params[:id])
+  end
+
+  def update
+    @worker = Worker.find(params[:id])
+    if @worker.update(worker_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+  def worker_params
+    params.require(:worker).permit(:worker_number, :name, :name_kana, :phone, :email, :password, :address_zip, :address, :warehouse_zip, :warehouse, :inaba, :yodo, :takubo, :ykkap, :sankyo, :lixil)
+  end
+
 end
