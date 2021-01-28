2f18bcb30aa2667caaeca3eb88db462213522a06
Tatsuya9156 <hmty9156@gmail.com>
2021-01-28 12:18:36 +0900

    社員モデルのバリデーションの設定

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,4 +3,15 @@ class Employee < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  with_options presence: true do
+    validates :name,      format: { with: /\A[ぁ-んァ-ン一-龠々]+\z/ }
+    validates :name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :admin
+    with_options numericality: { only_integer: true } do
+      validates :employee_number, format: { with: /\A[0-9]{,4}\z/ }
+      validates :phone,           format: { with: /\A[0-9]{10,11}\z/ }
+    end
+  end
+
 end
