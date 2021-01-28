5cd407a1925b3d08b7cd7ab3b063a6fd12f75c55
Tatsuya9156 <hmty9156@gmail.com>
2021-01-28 15:52:03 +0900

    不要コードの削除

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -4,7 +4,7 @@ class Worker < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  VALID_NUM_REGEX      = with: /\A[0-9]{4}\z/
+  VALID_NUM_REGEX      = /\A[0-9]{4}\z/
   VALID_NAME_REGEX     = /\A[ぁ-んァ-ン一-龠々]+\z/
   VALID_KANA_REGEX     = /\A[ァ-ヶー－]+\z/
   VALID_PHONE_REGEX    = /\A[0-9]{10,11}\z/
