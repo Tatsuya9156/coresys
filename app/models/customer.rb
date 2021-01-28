df4e25cef806d1bd1bccc44c4854f2d3524a7b0c
Tatsuya9156 <hmty9156@gmail.com>
2021-01-28 15:43:33 +0900

    顧客バリデーションの修正

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,8 +4,12 @@ class Customer < ApplicationRecord
   belongs_to :status
 
   VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龠々]+\z/
+<<<<<<< HEAD
   VALID_KANA_REGEX = /\A[ァ-ヶー－]+\z/
   VALID_PHONE_REGEX = /\A[0-9]{10,11}\z/
+=======
+  VALID_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/
+>>>>>>> parent of 9c9b463... 変数名の修正
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   VALID_ZIP_REGEX = /\A[0-9]{3}-[0-9]{4}\z/
 
