29c0ac91cdfecbe29b8ebc3c9a79c8b2c3ea9b2e
Tatsuya9156 <hmty9156@gmail.com>
2021-01-28 15:45:09 +0900

    インデント修正

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,15 +3,11 @@ class Customer < ApplicationRecord
   belongs_to :store
   belongs_to :status
 
-  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龠々]+\z/
-<<<<<<< HEAD
-  VALID_KANA_REGEX = /\A[ァ-ヶー－]+\z/
+  VALID_NAME_REGEX  = /\A[ぁ-んァ-ン一-龠々]+\z/
+  VALID_KANA_REGEX  = /\A[ァ-ヶー－]+\z/
   VALID_PHONE_REGEX = /\A[0-9]{10,11}\z/
-=======
-  VALID_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/
->>>>>>> parent of 9c9b463... 変数名の修正
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  VALID_ZIP_REGEX = /\A[0-9]{3}-[0-9]{4}\z/
+  VALID_ZIP_REGEX   = /\A[0-9]{3}-[0-9]{4}\z/
 
   with_options presence: true do
     validates :order_date
