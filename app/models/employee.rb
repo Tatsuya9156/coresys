ee5a890132e16ded09f81c1d3e3a53f4083bc548
Tatsuya9156 <hmty9156@gmail.com>
2021-01-28 15:54:35 +0900

    社員モデルのバリデーションの変数化

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,15 +3,18 @@ class Employee < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  VALID_NUM_REGEX   = /\A[0-9]{4}\z/
+  VALID_NAME_REGEX  = /\A[ぁ-んァ-ン一-龠々]+\z/
+  VALID_KANA_REGEX  = /\A[ァ-ヶー－]+\z/
+  VALID_PHONE_REGEX = /\A[0-9]{10,11}\z/
   
   with_options presence: true do
-    validates :name,      format: { with: /\A[ぁ-んァ-ン一-龠々]+\z/ }
-    validates :name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
-    validates :admin
-    with_options numericality: { only_integer: true } do
-      validates :employee_number, format: { with: /\A[0-9]{,4}\z/ }
-      validates :phone,           format: { with: /\A[0-9]{10,11}\z/ }
-    end
+    validates :employee_number, format: { with: VALID_NUM_REGEX }
+    validates :name,            format: { with: VALID_NAME_REGEX }
+    validates :name_kana,       format: { with: VALID_KANA_REGEX }
+    validates :phone,           format: { with: VALID_PHONE_REGEX }
+    validates :admin,           inclusion: {in: [true, false]}
   end
 
 end
