316ecaeadf953b80c741014a9f9dacaf903bbf37
Tatsuya9156 <hmty9156@gmail.com>
2021-01-28 15:50:01 +0900

    正規表現の変数化

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -4,17 +4,24 @@ class Worker < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
+  VALID_NUM_REGEX      = with: /\A[0-9]{4}\z/
+  VALID_NAME_REGEX     = /\A[ぁ-んァ-ン一-龠々]+\z/
+  VALID_KANA_REGEX     = /\A[ァ-ヶー－]+\z/
+  VALID_PHONE_REGEX    = /\A[0-9]{10,11}\z/
+  VALID_EMAIL_REGEX    = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_ZIP_REGEX      = /\A[0-9]{3}-[0-9]{4}\z/
+  VALID_DELICODE_REGEX = /\A[A-Z0-9]+\z/
+  
   with_options presence: true do
-    validates :name,      format: { with: /\A[ぁ-んァ-ン一-龠々]+\z/ }
-    validates :name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
-    validates :address_zip, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
+    validates :worker_number, format: { with: VALID_NUM_REGEX }
+    validates :name,          format: { with: VALID_NAME_REGEX }
+    validates :name_kana,     format: { with: VALID_NAME_KANA_REGEX }
+    validates :phone,         format: { with: VALID_PHONE_REGEX }
+    validates :address_zip,   format: { with: VALID_ZIP_REGEX }
     validates :address
-    with_options numericality: { only_integer: true } do
-      validates :worker_number, format: { with: /\A[0-9]{,4}\z/ }
-      validates :phone,         format: { with: /\A[0-9]{10,11}\z/ }
-    end
   end
-  with_options format: { with: /\A[A-Z0-9]+\z/ } do
+  validates :warehouse_zip, format: { with: VALID_ZIP_REGEX }
+  with_options format: { with: VALID_DELICODE_REGEX } do
     validates :inaba
     validates :yodo
     validates :takubo
@@ -22,6 +29,5 @@ class Worker < ApplicationRecord
     validates :sankyo
     validates :lixil
   end
-  validates :warehouse_zip, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
 
 end
