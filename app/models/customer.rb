78609a33760c3eec19e5d521b418e54b3ea35162
Tatsuya9156 <hmty9156@gmail.com>
2021-01-28 15:42:33 +0900

    修正

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,6 +5,7 @@ class Customer < ApplicationRecord
 
   VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龠々]+\z/
   VALID_KANA_REGEX = /\A[ァ-ヶー－]+\z/
+  VALID_PHONE_REGEX = /\A[0-9]{10,11}\z/
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   VALID_ZIP_REGEX = /\A[0-9]{3}-[0-9]{4}\z/
 
@@ -12,15 +13,13 @@ class Customer < ApplicationRecord
     validates :order_date
     validates :store_id
     validates :status_id
-    validates :name,      format: { with: VALID_NAME_REGEX }
-    validates :name_kana, format: { with: VALID_KANA_REGEX }
-    validates :email,     format: { with: VALID_EMAIL_REGEX }
-    validates :phone,     numericality: { only_integer: true }
-    with_options format: { with: VALID_ZIP_REGEX } do
-      validates :address_zip
-      validates :residence_zip
-    end
+    validates :name,        format: { with: VALID_NAME_REGEX }
+    validates :name_kana,   format: { with: VALID_KANA_REGEX }
+    validates :phone,       format: { with: VALID_PHONE_REGEX }
+    validates :email,       format: { with: VALID_EMAIL_REGEX }
+    validates :address_zip, format: { with: VALID_ZIP_REGEX }
     validates :address
-    validates :residence
   end
+  validates :residence_zip, format: { with: VALID_ZIP_REGEX }
+  validates :residence
 end
