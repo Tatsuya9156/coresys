c7520196e00ac69782e328ace27a08aee1c6cb09
Tatsuya9156 <hmty9156@gmail.com>
2021-01-29 11:37:13 +0900

    Factorybot修正

--- spec/factories/employees.rb
+++ spec/factories/employees.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :employee do
     employee_number       { Faker::Number.number(digits: 4) }
-    name                  { Gimai.kanji }
-    name_kana             { Gimai.katakana }
+    name                  { Gimei.last.kanji }
+    name_kana             { Gimei.last.katakana }
     position              { "部長" }
     phone                 { Faker::Number.number(digits: 11) }
     admin                 { false }
     email                 { Faker::Internet.free_email }
-    password              { Faker::Internet.password(min_length: 6) }
+    password              { '1a' + Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
   end
 end
