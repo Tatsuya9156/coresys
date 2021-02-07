01cc8da16183ae80d4c4ab9b88bcae899d90b1e8
Tatsuya9156 <hmty9156@gmail.com>
2021-02-07 20:50:23 +0900

    S3導入

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,15 @@ Employee.create!(
   email:           "test1@example.com",
   password:        "coresys0001"
 )
+
+Worker.create!(
+  worker_number: "1001",
+  name:          "田中太郎",
+  name_kana:     "タナカタロウ",
+  trade_name:    "田中工務店",
+  phone:         "08012345678",
+  email:         "testtest1@example.com",
+  password:      "coresys1001",
+  address_zip:   "123-4567",
+  address:       "愛知県名古屋市中区栄"
+)
