45ec35e3e61c0b62d81f811f4a6756462d01e095
Tatsuya9156 <hmty9156@gmail.com>
2021-02-13 00:11:41 +0900

    チャットコントローラーのnewアクションの削除

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,17 +1,14 @@
 class ChatsController < ApplicationController
   def index
-  end
-
-  def new
     @chat = Chat.new
   end
 
   def create
     @chat = Chat.new(chat_params)
     if @chat.save
-      redirect_to root_path
+      redirect_to chat_path(params[:id])
     else
-      render :new
+      render :index
     end
   end
 
