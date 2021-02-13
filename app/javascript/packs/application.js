c437d186c8cee58d4e2e47a0b333d7c4de550e39
Tatsuya9156 <hmty9156@gmail.com>
2021-02-13 20:36:13 +0900

    職人登録に関するJSファイル作成

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,8 @@ require("@rails/ujs").start()
 // require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("modal_windows")
+require("modal_window/chat_create")
+require("modal_window/worker_create")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
