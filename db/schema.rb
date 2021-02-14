554ded453f5b6928fa8f157c416e34d675a0a8a3
Tatsuya9156 <hmty9156@gmail.com>
2021-02-14 15:22:59 +0900

    seedファイル再作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,134 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_02_01_055248) do
 
+  create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "chat_employees", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "chat_id"
+    t.bigint "employee_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["chat_id"], name: "index_chat_employees_on_chat_id"
+    t.index ["employee_id"], name: "index_chat_employees_on_employee_id"
+  end
+
+  create_table "chat_workers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "chat_id"
+    t.bigint "worker_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["chat_id"], name: "index_chat_workers_on_chat_id"
+    t.index ["worker_id"], name: "index_chat_workers_on_worker_id"
+  end
+
+  create_table "chats", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "customers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.date "order_date", null: false
+    t.integer "store_id", null: false
+    t.integer "status_id", default: 1, null: false
+    t.string "name", null: false
+    t.string "name_kana", null: false
+    t.string "phone", null: false
+    t.string "email", null: false
+    t.string "address_zip", null: false
+    t.string "address", null: false
+    t.string "residence_zip", null: false
+    t.string "residence", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "employees", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "employee_number", null: false
+    t.string "name", null: false
+    t.string "name_kana", null: false
+    t.string "section", null: false
+    t.string "position", null: false
+    t.string "phone", null: false
+    t.boolean "admin", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_employees_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_employees_on_reset_password_token", unique: true
+  end
+
+  create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "text", null: false
+    t.bigint "chat_id"
+    t.bigint "employee_id"
+    t.bigint "worker_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["chat_id"], name: "index_messages_on_chat_id"
+    t.index ["employee_id"], name: "index_messages_on_employee_id"
+    t.index ["worker_id"], name: "index_messages_on_worker_id"
+  end
+
+  create_table "workers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "worker_number", null: false
+    t.string "name", null: false
+    t.string "name_kana", null: false
+    t.string "trade_name", null: false
+    t.string "phone", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "address_zip", null: false
+    t.string "address", null: false
+    t.string "warehouse_zip", null: false
+    t.string "warehouse", null: false
+    t.string "warehouse_info", null: false
+    t.string "inaba", null: false
+    t.string "yodo", null: false
+    t.string "takubo", null: false
+    t.string "ykkap", null: false
+    t.string "sankyo", null: false
+    t.string "lixil", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_workers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_workers_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "chat_employees", "chats"
+  add_foreign_key "chat_employees", "employees"
+  add_foreign_key "chat_workers", "chats"
+  add_foreign_key "chat_workers", "workers"
+  add_foreign_key "messages", "chats"
+  add_foreign_key "messages", "employees"
+  add_foreign_key "messages", "workers"
 end
