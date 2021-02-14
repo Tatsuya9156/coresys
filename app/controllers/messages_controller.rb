9498f75d2eeb2025ab9e775235ca46f62ff143d2
Tatsuya9156 <hmty9156@gmail.com>
2021-02-14 15:41:58 +0900

    条件式の記述変更

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,22 +1,13 @@
 class MessagesController < ApplicationController
   def create
-    if employee_signed_in?
-      @message = Message.new(employee_message_params)
+      @message = Message.new(employee_message_params) if employee_signed_in?
+      @message = Message.new(worker_message_params) if worker_signed_in?
       if @message.save
         redirect_to chat_path(params[:chat_id])
       else
         @chat_show = Chat.find(params[:chat_id])
         render 'chats/show'
       end
-    elsif worker_signed_in?
-      @message = Message.new(worker_message_params)
-      if @message.save
-        redirect_to chat_path(params[:chat_id])
-      else
-        @chat_show = Chat.find(params[:chat_id])
-        render 'chats/show'
-      end
-    end
   end
 
   private
