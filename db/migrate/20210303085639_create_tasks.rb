c776cac3b3b6a9269fdf19bab5979285ea213dd7
Tatsuya9156 <hmty9156@gmail.com>
2021-03-04 19:39:06 +0900

    テスト

--- db/migrate/20210303085639_create_tasks.rb
+++ db/migrate/20210303085639_create_tasks.rb
@@ -1,7 +1,7 @@
 class CreateTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks do |t|
-      t.references :chat,     foreign_key: true
+      t.references :chat,     foreign_key: truee
       t.datetime   :deadline, null: false
       t.text       :text,     null: false
       t.references :client,   foreign_key: {to_table: :employees}
