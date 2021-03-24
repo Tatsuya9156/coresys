322e7ebdd5f2dd7f4c74d3e59b912dcfd5a02040
Tatsuya9156 <hmty9156@gmail.com>
2021-03-24 20:48:59 +0900

    アコーディオンメニューで実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,6 +17,7 @@ require("form/customer_meeting_edit")
 require("form/customer_construction_create")
 require("form/customer_construction_edit")
 require("customer/status_color")
+require("sidebar/search_form")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
